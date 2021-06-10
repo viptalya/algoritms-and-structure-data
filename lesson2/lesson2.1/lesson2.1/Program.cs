359b44e1fdfe5b2a032141158b34a7ae500071de
vitalii <vip_10@bk.ru>
2021-06-10 09:50:47 +0300

    lesson2: Array, Node, Search

---
+++ lesson2/lesson2.1/lesson2.1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace lesson2._1
+{
+    //1. Двусвязный список
+    class Program
+    {
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node NextNode { get; set; }
+            public Node PrevNode { get; set; }
+            public int GetCount()
+            {
+                int count = 0;
+                Node nodeCount = this;
+                while (nodeCount.NextNode != null)
+                {
+                    count++;
+                    nodeCount = nodeCount.NextNode;
+                }
+                return ++count;
+            }
+            public void AddNode(int value)
+            {
+                Node nodeAdd = this;
+                while (nodeAdd.NextNode != null)
+                {
+                    nodeAdd = nodeAdd.NextNode;
+                }
+                Node nodeNewAdd = new Node();
+                nodeNewAdd.Value = value;
+                nodeNewAdd.NextNode = null;
+                nodeNewAdd.PrevNode = nodeAdd;
+                nodeAdd.NextNode = nodeNewAdd;
+            }
+            public void AddNodeAfter(Node node, int value)
+            {
+                Node nodeAfter = node.NextNode;
+                Node nodeNewAfter = new Node();
+                nodeNewAfter.Value = value;
+                nodeNewAfter.NextNode = nodeAfter;
+                nodeNewAfter.PrevNode = node;
+                node.NextNode = nodeNewAfter;
+                if (nodeAfter != null)
+                {
+                    nodeAfter.PrevNode = nodeNewAfter;
+                }
+            }
+            public void RemoveNode(int index)
+            {
+                Node nodeIndex = this;
+                while (index != 0)
+                {
+                    if (nodeIndex.NextNode == null)
+                        return;
+                    nodeIndex = nodeIndex.NextNode;
+                    index--;
+                }
+                RemoveNode(nodeIndex);
+            }
+            public void RemoveNode(Node node)
+            {
+                if (node.PrevNode == null)
+                {
+                    node.Value = node.NextNode.Value;
+                    node.NextNode = node.NextNode.NextNode;
+                    return;
+                }
+                node.PrevNode.NextNode = node.NextNode;
+                if (node.NextNode != null)
+                    node.NextNode.PrevNode = node.PrevNode;
+            }
+            public Node FindNode(int searchValue)
+            {
+                Node nodeSearch = this;
+                while (true)
+                {
+                    if (nodeSearch.Value == searchValue)
+                    {
+                        break;
+                    }
+                    nodeSearch = nodeSearch.NextNode;
+
+                }
+                return nodeSearch;
+            }
+        }
+        public interface ILinkedList
+        {
+            int GetCount();
+            void AddNode(int value);
+            void AddNodeAfter(Node node, int value);
+            void RemoveNode(int index);
+            void RemoveNode(Node node);
+            Node FindNode(int searchValue);
+        }
+
+        static void PrintNode(Node nodePrint)
+        {
+            Node node = nodePrint;
+            while (node.NextNode != null)
+            {
+                Console.Write(node.Value);
+                node = node.NextNode;
+            }
+            Console.WriteLine(node.Value);
+        }
+        static void Main(string[] args)
+        {
+            Node node = new Node();
+            node.Value = 1;
+            node.AddNode(5);
+            PrintNode(node);
+            node.AddNodeAfter(node.NextNode, 4);
+            PrintNode(node);
+            node.RemoveNode(0);
+            PrintNode(node);
+            Console.WriteLine($"Кол-во значений: {node.GetCount()}");
+            Console.WriteLine($"Искомое значение: {node.FindNode(5).Value}");
+            node.RemoveNode(node);
+            PrintNode(node);
+        }
+    }
+}
---
+++ lesson2/lesson2.2/lesson2.2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lesson2._2
+{
+    //2. Двоичный поиск
+    class Program
+    {
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0; //о(1)
+            int max = inputArray.Length - 1; //о(1)
+            while (min <= max) //о(2n^2)
+            {
+                int mid = (min + max) / 2; //o(1)
+                if (searchValue == inputArray[mid]) //о(2n)
+                {
+                    return mid; //o(1)
+                }
+                else if (searchValue < inputArray[mid]) //о(2n)
+                {
+                    max = mid - 1; //o(1)
+                }
+                else
+                {
+                    min = mid + 1; //o(1)
+                }
+            }
+            return -1; //o(1)
+        }
+        //Сложность: O(6) + O(N^2) + O(4N) 
+        public class TestCase
+        {
+            public int x { get; set; }
+            public int res { get; set; }
+            public Exception exception { get; set; }
+        }
+
+        static void TestSearch(TestCase testCase)
+        {
+            try
+            {
+                int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                var actual = BinarySearch(arr, testCase.x);
+                if (actual == testCase.res)
+                {
+                    Console.WriteLine($"Значение: {testCase.x}, " +
+                        $"номер: {actual}");
+                    Console.WriteLine("Valid test");
+                }
+                else
+                {
+                    Console.WriteLine($"Значение: {testCase.x}, " +
+                        $"номер: {actual}");
+                    Console.WriteLine("Invalid test");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.exception != null)
+                {
+                    Console.WriteLine("Valid test");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid test");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var testCase1 = new TestCase()
+            {
+                x = 6,
+                res = 5,
+                exception = null
+            };
+            TestSearch(testCase1);
+            var testCase2 = new TestCase()
+            {
+                x = 1,
+                res = 0,
+                exception = null
+            };
+            TestSearch(testCase2);
+            var testCase3 = new TestCase()
+            {
+                x = 9,
+                res = 8,
+                exception = null
+            };
+            TestSearch(testCase3);
+            var testCase4 = new TestCase()
+            {
+                x = 5,
+                res = 3,
+                exception = null
+            };
+            TestSearch(testCase4);
+            var testCase5 = new TestCase()
+            {
+                x = 3,
+                res = 5,
+                exception = null
+            };
+            TestSearch(testCase5);
+        }
+    }
+}
